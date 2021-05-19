9f80fd7af5e9b5b18a08519ee804814d74e93b11
Meng Fu <Ellie@Ellies-Air.lan1>
2021-05-18 19:16:24 -0500

    pushing update

--- routes/books.js
+++ routes/books.js
@@ -2,10 +2,10 @@ const { kMaxLength } = require('buffer');
 const express = require ('express');
 const router = express.Router();
 const controller = require("../controller/booksCtrl.js");
-const controller = require("../controller/usersCtrl.js");
+// const controller = require("../controller/usersCtrl.js");
 
 //list users 
-router.get("/allusers", controller.allusers)
+// router.get("/allusers", controller.allusers)
 
 //list all books list
 router.get("/allBooks", controller.allBooks)
