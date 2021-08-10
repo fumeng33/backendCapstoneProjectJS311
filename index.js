7918ffb6bba5417ef93e7e9a0368da14024caf4f
Meng Fu <Ellie@Ellies-MacBook-Air.local>
2021-08-10 09:13:01 -0500

    books

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 const bookRouter = require('./routes/books');
 app.use(express.static("build"));
 app.use(function(req, res, next){
--- routes/books.js
+++ routes/books.js
@@ -3,18 +3,18 @@ const express = require ('express');
 const router = express.Router();
 const controller = require("../controller/booksCtrl.js");
 
+
 // const controller = require("../controller/usersCtrl.js");
 
 //list users 
 // router.get("/allusers", controller.allusers)
 
 //list all books list
-router.get("/", controller.allBooks)
+router.get("/books", controller.allBooks)
 
 //list books
 router.get("/books/:id", controller.booksById)
-router.get("/genre", controller.genre)
-router.get("/authors", controller.authors)
+router.get("/title", controller.title)
 
 //edit
 router.put("/books/:id", controller.editBookById)
